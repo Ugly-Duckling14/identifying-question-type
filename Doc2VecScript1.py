635d02512db13b030659546df297b5613f6da64b
Munawir Elambilat <54879835+Ugly-Duckling14@users.noreply.github.com>
2020-10-13 16:44:51 +0530

    Update Doc2VecScript1.py

--- Doc2VecScript1.py
+++ Doc2VecScript1.py
@@ -16,12 +16,12 @@ question_dir = 'LabelledData.txt'
 questions = []
 types = []
 with io.open(question_dir, errors='ignore', encoding='utf-8') as fid:
-	for line in fid:
-		question, type = line.split(",,,")
-		question = question.strip()
-		type = type.strip()
-		questions.append(question)
-		types.append(type)
+    for line in fid:
+        question, type = line.split(",,,")
+        question = question.strip()
+        type = type.strip()
+        questions.append(question)
+        types.append(type)
 
 ############################# Preprocessing Questions #############################
 
@@ -29,8 +29,8 @@ with io.open(question_dir, errors='ignore', encoding='utf-8') as fid:
 nlp = spacy.load('en')
 processed_questions = []
 for question in nlp.pipe(questions, n_threads=4, batch_size=10):
-	question = [token.lemma_ for token in question if token.is_alpha and len(token)>1]
-	processed_questions.append(question)
+    question = [token.lemma_ for token in question if token.is_alpha and len(token)>1]
+    processed_questions.append(question)
 
 questions = processed_questions
 del processed_questions
@@ -42,18 +42,18 @@ from gensim.models import Phrases
 bigram = Phrases(questions, min_count=3)
 
 for idx in range(len(questions)):
-	for token in bigram[questions[idx]]:
-		if '_' in token:
-			# Token is a bigram, add to question.
-			questions[idx].append(token)
-			
+    for token in bigram[questions[idx]]:
+        if '_' in token:
+            # Token is a bigram, add to question.
+            questions[idx].append(token)
+
 
 processed_questions = []
 analyzedDocument = namedtuple('AnalyzedDocument', 'words tags')
 
 for i, text in enumerate(questions):
-	tags = [i]
-	processed_questions.append(analyzedDocument(text, tags))
+    tags = [i]
+    processed_questions.append(analyzedDocument(text, tags))
 
 questions = processed_questions
 del processed_questions
@@ -78,8 +78,8 @@ questions_vec = []
 
 #storing all vectors of questions
 for i in range(len(questions)):
-	vec = model.docvecs[i]
-	questions_vec.append(vec)
+    vec = model.docvecs[i]
+    questions_vec.append(vec)
 
 ############################# create labels ###################################
 
@@ -92,26 +92,26 @@ n_labels = len(question_type)
 questions_label = np.zeros((n_samples,  n_labels))
 
 for i, type in enumerate(types):
-	questions_label[i][question_type.index(type)] = 1
+    questions_label[i][question_type.index(type)] = 1
 
 
 ############################# create training and test data ###################################
 
 def get_train_test_inds(y,train_proportion=0.8):
-	train_inds = np.zeros(len(y),dtype=bool)
-    	test_inds = np.zeros(len(y),dtype=bool)
-	values = list(set(y))
-	for value in values:
-        	value_inds = []
-        	for i, x in enumerate(y):
-			if x == value:
-				value_inds.append(i)
-		np.random.shuffle(value_inds)
-        	n = int(train_proportion*len(value_inds))
-		train_inds[value_inds[:n]]=True
-        	test_inds[value_inds[n:]]=True
+    train_inds = np.zeros(len(y),dtype=bool)
+    test_inds = np.zeros(len(y),dtype=bool)
+    values = list(set(y))
+    for value in values:
+            value_inds = []
+            for i, x in enumerate(y):
+                if x == value:
+                    value_inds.append(i)
+            np.random.shuffle(value_inds)
+            n = int(train_proportion*len(value_inds))
+            train_inds[value_inds[:n]]=True
+            test_inds[value_inds[n:]]=True
 
-	return train_inds,test_inds
+    return train_inds,test_inds
 
 train_inds,test_inds = get_train_test_inds(types)
 
@@ -119,17 +119,17 @@ train_ques = []
 train_label = []
 
 for i, idx in enumerate(train_inds):
-	if idx==True:
-		train_ques.append(questions_vec[i])
-		train_label.append(questions_label[i])
+    if idx==True:
+        train_ques.append(questions_vec[i])
+        train_label.append(questions_label[i])
 
 test_ques = []
 test_label = []
 
 for i, idx in enumerate(test_inds):
-	if idx==True:
-		test_ques.append(questions_vec[i])
-		test_label.append(questions_label[i])
+    if idx==True:
+        test_ques.append(questions_vec[i])
+        test_label.append(questions_label[i])
 
 test_true = tf.placeholder(tf.float32, shape=[None, n_labels], name='test_true')
 
@@ -193,50 +193,49 @@ session.run(tf.global_variables_initializer())
 
 train_batch_size = 64
 
-########################  Helper-function to perform optimization iterations ####################### 
+########################  Helper-function to perform optimization iterations #######################
 
 # Counter for total number of iterations performed so far.
 total_iterations = 0
 
 def optimize(num_iterations):
-	# Ensure we update the global variable rather than a local copy.
-   	global total_iterations
+    # Ensure we update the global variable rather than a local copy.
+       global total_iterations
+       # Start-time used for printing time-usage below.
+       start_time = time.time()
 
-    	# Start-time used for printing time-usage below.
-    	start_time = time.time()
+       for i in range(total_iterations, total_iterations + num_iterations):
+            # Put the batch into a dict with the proper names
+            # for placeholder variables in the TensorFlow graph.
+            feed_dict_train = {x: train_ques, y_true: train_label}
 
-   	for i in range(total_iterations, total_iterations + num_iterations):
-        	# Put the batch into a dict with the proper names
-        	# for placeholder variables in the TensorFlow graph.
-        	feed_dict_train = {x: train_ques, y_true: train_label}
+            # Run the optimizer using this batch of training data.
+            # TensorFlow assigns the variables in feed_dict_train
+            # to the placeholder variables and then runs the optimizer.
+            session.run(optimizer, feed_dict=feed_dict_train)
 
-        	# Run the optimizer using this batch of training data.
-        	# TensorFlow assigns the variables in feed_dict_train
-        	# to the placeholder variables and then runs the optimizer.
-        	session.run(optimizer, feed_dict=feed_dict_train)
+            # Print status every 100 iterations.
+            if i % 100 == 0:
+                    # Calculate the accuracy on the training-set.
+                    acc = session.run(accuracy, feed_dict=feed_dict_train)
 
-        	# Print status every 100 iterations.
-        	if i % 100 == 0:
-            		# Calculate the accuracy on the training-set.
-            		acc = session.run(accuracy, feed_dict=feed_dict_train)
+                    # Message for printing.
+                    msg = "Optimization Iteration: {0:>6}, Training Accuracy: {1:>6.1%}"
 
-            		# Message for printing.
-            		msg = "Optimization Iteration: {0:>6}, Training Accuracy: {1:>6.1%}"
+                    # Print it.
+                    print(msg.format(i + 1, acc))
 
-            		# Print it.
-            		print(msg.format(i + 1, acc))
+        # Update the total number of iterations performed.
+       total_iterations += num_iterations
 
-    	# Update the total number of iterations performed.
-    	total_iterations += num_iterations
+        # Ending time.
+       end_time = time.time()
 
-    	# Ending time.
-    	end_time = time.time()
+        # Difference between start and end-times.
+       time_dif = end_time - start_time
 
-    	# Difference between start and end-times.
-    	time_dif = end_time - start_time
-
-    	# Print the time-usage.
-    	print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
+        # Print the time-usage.
+       print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
 
 
 ######################## Helper-function for showing the classification accuracy #######################
@@ -245,40 +244,40 @@ def optimize(num_iterations):
 test_batch_size = 10
 
 def print_test_accuracy():
-		# Number of question in the test-set.
-    	num_test = len(test_ques)
+        # Number of question in the test-set.
+        num_test = len(test_ques)
 
-    	# Allocate an array for the predicted classes which
-    	# will be calculated in batches and filled into this array.
-    	cls_pred = np.zeros(shape=num_test, dtype=np.int)
+        # Allocate an array for the predicted classes which
+        # will be calculated in batches and filled into this array.
+        cls_pred = np.zeros(shape=num_test, dtype=np.int)
 
-    	# Now calculate the predicted classes for the batches.
-    	# We will just iterate through all the batches.
-    	# There might be a more clever and Pythonic way of doing this.
-	
-	
-    	# The starting index for the next batch is denoted i.
-	feed_dict = {x: test_ques, y_true: test_label}
-	cls_pred = session.run(y_pred_cls, feed_dict=feed_dict)
+        # Now calculate the predicted classes for the batches.
+        # We will just iterate through all the batches.
+        # There might be a more clever and Pythonic way of doing this.
 
-    	# Convenience variable for the true class-numbers of the test-set.
-	feed_dict = {test_true: test_label}
-	cls_true = session.run(test_true_cls, feed_dict=feed_dict)
 
-    	# Create a boolean array whether each question is correctly classified.
-    	correct = (cls_true == cls_pred)
+        # The starting index for the next batch is denoted i.
+        feed_dict = {x: test_ques, y_true: test_label}
+        cls_pred = session.run(y_pred_cls, feed_dict=feed_dict)
 
-    	# Calculate the number of correctly classified questions.
-    	# When summing a boolean array, False means 0 and True means 1.
-    	correct_sum = correct.sum()
+        # Convenience variable for the true class-numbers of the test-set.
+        feed_dict = {test_true: test_label}
+        cls_true = session.run(test_true_cls, feed_dict=feed_dict)
 
-    	# Classification accuracy is the number of correctly classified
-    	# questions divided by the total number of questions in the test-set.
-    	acc = float(correct_sum) / num_test
+        # Create a boolean array whether each question is correctly classified.
+        correct = (cls_true == cls_pred)
 
-    	# Print the accuracy.
-    	msg = "Accuracy on Test-Set: {0:.1%} ({1} / {2})"
-    	print(msg.format(acc, correct_sum, num_test))
+        # Calculate the number of correctly classified questions.
+        # When summing a boolean array, False means 0 and True means 1.
+        correct_sum = correct.sum()
+
+        # Classification accuracy is the number of correctly classified
+        # questions divided by the total number of questions in the test-set.
+        acc = float(correct_sum) / num_test
+
+        # Print the accuracy.
+        msg = "Accuracy on Test-Set: {0:.1%} ({1} / {2})"
+        print(msg.format(acc, correct_sum, num_test))
 
 ######################## Running the model and Checking Accuracy #######################
 
@@ -298,7 +297,7 @@ print_test_accuracy()
 
 #Performance after 1000 optimization iterations
 optimize(num_iterations=900)  #We performed 100 iterations above.
-print_test_accuracy() 
+print_test_accuracy()
 
 #Performance after 10000 optimization iterations
 #optimize(num_iterations=9000)  #We performed 1000 iterations above.
@@ -308,14 +307,13 @@ print_test_accuracy()
 
 choice = 'yes'
 while choice=='yes' or choice=='y' :
-	print "Enter a Question"
-	input1 = raw_input().decode('utf8')
-	question = nlp(input1)
-	question = [token.lemma_ for token in question if token.is_alpha and len(token)>1]
-	ques_vec = [model.infer_vector(question)]
-	feed_dict = {x: ques_vec}
-	cls_pred = session.run(y_pred_cls, feed_dict=feed_dict)
-	print "Question Type: ", question_type[cls_pred[0]]
-	print "Want to check again (y/n)"
-	choice = raw_input()
-	
+    print("Enter a Question")
+    input1 = raw_input().decode('utf8')
+    question = nlp(input1)
+    question = [token.lemma_ for token in question if token.is_alpha and len(token)>1]
+    ques_vec = [model.infer_vector(question)]
+    feed_dict = {x: ques_vec}
+    cls_pred = session.run(y_pred_cls, feed_dict=feed_dict)
+    print("Question Type: ", question_type[cls_pred[0]])
+    print("Want to check again (y/n)")
+    choice = raw_input()
